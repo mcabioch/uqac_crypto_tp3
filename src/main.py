db9852f49e9bccca73034f6ab5152b08f186b3b1
Forenceur <battaglia.eddy@gmail.com>
2019-11-08 16:42:29 +0100

    fin

--- src/main.py
+++ src/main.py
@@ -8,8 +8,8 @@ import MillerRabin
 factors=[int(nb) for nb in range(3,10000,2) if MillerRabin.is_Prime(nb)]
 
 def main():
-    lenNb=random.randint(5,20)
-    eps=random.randint(1,10)
+    lenNb=random.randint(5,8)
+    eps=random.randint(1,3)
     
     p=gen_RSApq(lenNb)
     q=gen_RSApq(lenNb+eps)
@@ -17,7 +17,7 @@ def main():
     e=gen_exp(p,q)
     d=congru_equation(e,1,(p-1)*(q-1))
     
-    m=random.randint(10**100,10**200)
+    m=random.randint(n/10,n)
     print(m)
     c=expModulaire(m,e,n)
     plain=expModulaire(c,d,n)
@@ -33,57 +33,61 @@ def gen_exp(p,q):
     return e
 
 def gen_RSApq(n):
-    x=gen_prime(n)
-    while(not test_sfactor(x,n)):
-        x=gen_prime(n)
+    x0=gen_prime(n//3)
+    a=10**(n-n//3)
+    b=a*100
+    #on genere un grand nombre premier
+    #puis on en cherche un de la forme k*x+1
+    #ainsi p-1 ne sera pas seulement composé de petits facteurs
+    k=random.randrange(a,b)
+    x=k*x0+1
+    x|=1
+    congruCompo=pow(2,x-1,x)
+    while(congruCompo!=1 or not MillerRabin.is_Prime(x)):
+        while(there_is_sfactor(x)):
+            x+=x0
+        congruCompo=pow(2,x-1,x)
     return x
     
-def test_sfactor(x,n):
-    prod=1
-    for nb in factors:
-        prod*=math.gcd(pow(nb,n//2),x)
-    return x/prod > 10**50
-
 def gen_prime(n):
     x=0
-    if(n<5):
-        n=5
+    if(n<1):
+        n=1
     a=10**n
     b=a*100
     x=random.randrange(a,b)
     x|=1
     congruCompo=pow(2,x-1,x)
     while(congruCompo!=1 or not MillerRabin.is_Prime(x)):
+        #es qu'on peut facilement diviser ce nombre (petit facteur)
         while(there_is_sfactor(x)):
-            x+=random.randrange(2,10)
-            x|=1
+            x+=2
         congruCompo=pow(2,x-1,x)
     return x
 
+def factorPminus1(n):
+    B=20
+    a=2
+    b=a%n
+    b=pow(b,math.factorial(B),n)
+    d=math.gcd(b-1,n)
+    return 1<d and d<n
+
 def there_is_sfactor(x):
+    #si x est pair
     if(not x&1):
         return True
+    #si x peut être divisé par un des petits nombres premiers
     for nb in factors:
         if(x%nb==0):
             return True
     return False
 
-def expModulaire_v1(x,puissance,modulo):
-    exp=1
-    congruXPuissExp=x
-    xPuissExp=x
-    congruCompo=1
-    while(exp<puissance):
-        if(exp & puissance):
-            congruCompo=(congruCompo*congruXPuissExp)%modulo
-        xPuissExp*=xPuissExp
-        exp*=2
-        congruXPuissExp=(congruXPuissExp**2)%modulo
-    return congruCompo
-
 def expModulaire(x,puissance,modulo):
     x=x%modulo
     congruCompo=1
+    #on exploite ici le fait que l'on calcule tous les (2^2^n)
+    #et que l'on a la décomposition en base 2 de puissance
     while(puissance>0):
         if(puissance & 1):
             congruCompo=(congruCompo*x)%modulo
